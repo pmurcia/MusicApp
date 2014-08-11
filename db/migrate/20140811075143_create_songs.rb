d72e795e2e2f57d23c5d9e007042fedcdadafec5
vagrant <vagrant@ironhack>
2014-08-11 08:08:22 +0000

    Added default song

---
+++ db/migrate/20140811075143_create_songs.rb
@@ -0,0 +1,19 @@
+class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.string :url
+      t.string :image
+      t.string :cathegory
+      t.string :tag1, default: nil
+      t.string :tag2, default: nil
+      t.string :tag3, default: nil
+      t.string :tag4, default: nil
+      t.string :tag5, default: nil
+      t.string :tag6, default: nil
+      t.string :tag7, default: nil
+      t.string :tag8, default: nil
+      # t.timestamps
+    end
+  end
+end
