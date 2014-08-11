2f8422641894cd9bc25bcecef2d2d9115fd01329
vagrant <vagrant@ironhack>
2014-08-11 15:28:10 +0000

    Added song

---
+++ test/controllers/piano_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PianoControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/helpers/piano_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class PianoHelperTest < ActionView::TestCase
+end
